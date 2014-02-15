5ceaedb1e22e01e0045ebaaf5851ea22702a5247
eceeb <dev.mail@icloud.com>
2014-02-15 17:44:53 +0100

    initial commit

---
+++ moonshooter-desktop/src/com/ep/moonshooter/Main.java
@@ -0,0 +1,16 @@
+package com.ep.moonshooter;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "moonshooter";
+		cfg.useGL20 = false;
+		cfg.width = 480;
+		cfg.height = 320;
+		
+		new LwjglApplication(new MyGdxGame(), cfg);
+	}
+}
